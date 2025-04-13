d7478f377721e4dc16655f03cb29456e89488c4d
wizzylinc <mikekaroki47@gmail.com>
2025-04-13 23:17:20 +0300

    COMMIWT

--- app/src/main/java/com/mike/sokomart/ui/screens/form/FormScreen.kt
+++ app/src/main/java/com/mike/sokomart/ui/screens/form/FormScreen.kt
@@ -1,5 +1,7 @@
 package com.mike.sokomart.ui.screens.form
 
+import android.content.Intent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -7,10 +9,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -23,15 +31,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.mike.sokomart.R
 import com.mike.sokomart.ui.theme.Purple80
-
+import com.mike.sokomart.ui.theme.Purple80
 
 @Composable
 fun FormScreen(navController: NavController){
@@ -39,53 +49,138 @@ fun FormScreen(navController: NavController){
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
-    )
-    {
-        Text(
-            text = "CREATE AN ACCOUNT",
-            fontSize = 30.sp
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.img),
+            contentDescription = "home",
+            modifier = Modifier.size(250.dp),
         )
-        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = "Sign Up",
+            fontSize = 50.sp,
+            color = Purple80
+        )
+        Spacer(modifier = Modifier.height(10.dp))
 
-        //Variables
+        //variables
         var fullname by remember { mutableStateOf("") }
         var username by remember { mutableStateOf("") }
         var email by remember { mutableStateOf("") }
         var password by remember { mutableStateOf("") }
 
-        //Fullname
+        //fullname
         OutlinedTextField(
             value = fullname,
             onValueChange = { fullname = it },
-            modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
-            leadingIcon = { Icon(imageVector = Icons.Default.Person, contentDescription = "", tint = Color.Green) },
-            label = {Text(text = "Fullname")},
+            modifier = Modifier.fillMaxWidth().padding(start = 15.dp, end = 15.dp),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "",
+                    tint = Purple80,
+                )
+            },
+            label = {
+                Text(
+                    text = "Full Name"
+                )
+            },
             colors = OutlinedTextFieldDefaults.colors(
                 focusedBorderColor = Purple80,
-                unfocusedBorderColor = Purple80
+                unfocusedBorderColor = Purple80,
+                focusedLabelColor = Purple80,
+                unfocusedLabelColor = Purple80,
+                cursorColor = Purple80
             ),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
-        )
-        //End of fullname
-        Spacer(modifier = Modifier.height(20.dp))
 
-        //Email
+        )
+        //end of fullname
+        //username
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            modifier = Modifier.fillMaxWidth().padding(start = 15.dp, end = 15.dp),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Face,
+                    contentDescription = "",
+                    tint = Purple80,
+                )
+            },
+            label = {Text(text = "Username")},
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Purple80,
+                unfocusedBorderColor = Purple80,
+                focusedLabelColor = Purple80,
+                unfocusedLabelColor = Purple80,
+                cursorColor = Purple80
+            ),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+
+        )
+        //end of username
+        //email
         OutlinedTextField(
             value = email,
-            onValueChange = { email= it },
-            modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
-            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "", tint = Color.Green) },
+            onValueChange = { email = it },
+            modifier = Modifier.fillMaxWidth().padding(start = 15.dp, end = 15.dp),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = "",
+                    tint = Purple80,
+                )
+            },
             label = {Text(text = "Email")},
             colors = OutlinedTextFieldDefaults.colors(
                 focusedBorderColor = Purple80,
-                unfocusedBorderColor = Purple80
+                unfocusedBorderColor = Purple80,
+                focusedLabelColor = Purple80,
+                unfocusedLabelColor = Purple80,
+                cursorColor = Purple80
             ),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+
         )
-        //End of email
+        //end of email
+        //password
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            modifier = Modifier.fillMaxWidth().padding(start = 15.dp, end = 15.dp),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Lock,
+                    contentDescription = "",
+                    tint = Purple80,
+                )
+            },
+            label = {Text(text = "Password")},
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Purple80,
+                unfocusedBorderColor = Purple80,
+                focusedLabelColor = Purple80,
+                unfocusedLabelColor = Purple80,
+                cursorColor = Purple80
+            ),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+
+        )
+        //end of password
+        Spacer(modifier = Modifier.height(10.dp))
+        //buton start
+        Button(
+            onClick = {},
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(Purple80),
+            modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp)
+        ) {
+            Text(text = "Register")
+        }
+        //buton end
     }
 }
-
 @Preview(showBackground = true)
 @Composable
 fun FormScreenPreview(){
